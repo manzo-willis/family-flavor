cd8de082f672b017e8a1cde2c76f441f30d8ac17
manzo-willis <121339104+manzo-willis@users.noreply.github.com>
2024-05-07 09:50:44 +0100

    logo testing

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,10 +2,8 @@ module.exports = {
     title: "Family flavour",
     url: "https://family-flavour.netlify.app/",
     language: "en",
-    description: "I am writing about my experiences as a naval navel-gazer.",
+    description: "I am writing about my experiences as a webdev and amateur cook.",
     author: {
-        name: "Your Name Here",
-        email: "youremailaddress@example.com",
-        url: "https://example.com/about-me/"
+        name: "Manzo Willis",
     }
 }
